20e8290676bfe39e2eebc0b8470d8fe96b7b8bfb
ShakhawatHossainShopon <shoponhossain32186@gmail.com>
2025-01-13 23:30:14 +0600

    bug fixed

--- src/screens/Home/sections/Card-section/CardSection.jsx
+++ src/screens/Home/sections/Card-section/CardSection.jsx
@@ -1,7 +1,7 @@
 import Card from "@/components/Card/Card";
 import card1 from "@/assets/banner-1.png";
 import card2 from "@/assets/banner-3.png";
-import card3 from "@/assets/banner-3.png";
+import card3 from "@/assets/banner-1.png";
 const CardSection = () => {
   return (
     <div>
